f3c498e169f065b02033dd24b2cf1169ba03facd
Nelson Lai <me@nelsonlai.me>
2025-09-01 12:20:09 +0800

    refactor: fix typos

--- index.test.ts
+++ index.test.ts
@@ -196,7 +196,7 @@ test('case: context menu subtrigger', () => {
   expect(groupTailwindClasses(input)).toEqual(expected)
 })
 
-test('case: context menu subcontent', () => {
+test('case: context menu sub-content', () => {
   const input =
     'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--radix-context-menu-content-transform-origin) overflow-hidden rounded-md border p-1 shadow-lg'
 
@@ -515,7 +515,7 @@ test('case: menubar item', () => {
   expect(groupTailwindClasses(input)).toEqual(expected)
 })
 
-test('case: menubar chcekbox item', () => {
+test('case: menubar checkbox item', () => {
   const input =
     "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-xs py-1.5 pr-2 pl-8 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
   const expected = [
