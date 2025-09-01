8e7b1d391e7e27e0b04cc7ca9335e55b60265d05
Nelson Lai <me@nelsonlai.me>
2025-09-01 12:23:01 +0800

    refactor: move to src, fix typos, add cspell config

--- src/index.test.ts
+++ src/index.test.ts
@@ -400,7 +400,7 @@ test('case: dropdown menu subtrigger', () => {
   expect(groupTailwindClasses(input)).toEqual(expected)
 })
 
-test('case: dropdown menu subcontent', () => {
+test('case: dropdown menu sub-content', () => {
   const input =
     'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-hidden rounded-md border p-1 shadow-lg'
   const expected = [
@@ -553,7 +553,7 @@ test('case: menubar subtrigger', () => {
   expect(groupTailwindClasses(input)).toEqual(expected)
 })
 
-test('case: menubar subcontent', () => {
+test('case: menubar sub-content', () => {
   const input =
     'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--radix-menubar-content-transform-origin) overflow-hidden rounded-md border p-1 shadow-lg'
   const expected = [
